5cce36ff4205954fe02e0637cc8bde08cc498e20
Damien <damien@okvitae.com>
2017-12-30 16:46:06 -0800

    Adding Mention Script
    Building out functionality to find Twitter users who have an interest in music, and mentioning them with the #1 billboard hit on the day that they created their Twitter account.

---
+++ billboard-ghost-mention-script.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#This script is in beta. 
+
+import tweepy, time, sys, cookielib, json
+from urllib2 import Request, urlopen, URLError
+from random import randint
+from datetime import datetime
+
+#enter the corresponding information from your Twitter application:
+CONSUMER_KEY = 'YOUR CONSUMER KEY HERE'
+CONSUMER_SECRET = 'YOUR CONSUMER SECRET HERE'
+ACCESS_KEY = 'YOUR ACCESS KEY HERE'
+ACCESS_SECRET = 'YOUR ACCESS SECRET HERE'
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+api = tweepy.API(auth)
+
+tryUser = 'THIS VARIABLE SHOULD BE EQUAL TO A TWITTER USERNAME WITHOUT THE @'
+
+api = tweepy.API(auth_handler=auth, parser=tweepy.parsers.JSONParser())
+user_data = api.get_user(tryUser) 
+createDate = user_data['created_at']
+
+months = [0, 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+
+#example date Fri Jun 15 08:38:53 +0000 2012
+
+createYear = (createDate[-4:])
+year = createYear
+
+createMonth = (createDate[4:7])
+thismonth = months.index(createMonth)
+month = ('{:02d}'.format(thismonth))
+
+createDay = (createDate[8:10])
+day = createDay
+
+if int(year) < 2015:
+	rawDate = str(year) + '-' + str(month) + '-' + str(day)
+else:
+	sys.exit()
+
+date = datetime.strptime(rawDate, '%Y-%m-%d').date()
+
+def lastSat(input):
+		d = input.toordinal()
+		last = d - 6
+		sunday = last - (last % 7)
+		saturday = sunday + 6
+		global newDate
+		newDate = date.fromordinal(saturday)
+		print newDate
+lastSat(date)
+hdr = {
+	   'User-Agent': 'Mozilla/5.0 (Android 4.4; Mobile; rv:41.0) Gecko/41.0 Firefox/41.0',
+	   'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+	   'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
+	   'Accept-Encoding': 'none',
+	   'Accept-Language': 'en-US,en;q=0.8',
+	   'Connection': 'keep-alive'
+	   }
+print str(newDate)
+request = Request('http://billboard.modulo.site/charts/' + str(newDate) + '?max=1', headers=hdr)
+try:
+	response = urlopen(request)
+	data = json.load(response)
+	artist = data['songs'][0]['display_artist']
+	songTitle = data['songs'][0]['song_name']
+	spotifyID = data['songs'][0]['spotify_id']
+	print artist, songTitle, spotifyID
+	api.update_status('@' + tryUser + ' The number 1 song on the Billboard Hot 100 on the day that you joined Twitter (' + str(newDate) + ') was "' + str(songTitle) + '" by ' + str(artist) + '! Listen on Spotify: https://open.spotify.com/track/' + str(spotifyID))
+except URLError, e:
+	print 'No data. Got an error code:', e, e.read()	
+	
+
+sys.exit()
